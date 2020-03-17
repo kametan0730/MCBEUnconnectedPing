2b0b5cb0e5d557a2c3d4daaf3cf831ba7d719c37
KAMETAN0730 <kametan0730@gmail.com>
2020-03-17 13:21:43 +0900

    変数名などの変更

--- ping.php
+++ ping.php
@@ -11,6 +11,8 @@
 const ID_UNCONNECTED_PING = 0x01;
 const ID_UNCONNECTED_PONG = 0x1c;
 
+const MAGIC = "\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78";
+
 function unconnectedPing(string $host, int $port, &$result) : bool{
 	$sock = @fsockopen("udp://" . $host, $port);
 	if (!$sock) false;
@@ -18,8 +20,8 @@ function unconnectedPing(string $host, int $port, &$result) : bool{
 
 	/* Encode UnconnectedPing */
 	$sendPacketBuffer = chr(ID_UNCONNECTED_PING); // UnconnectedPing packet id
-	$sendPacketBuffer .= pack("J", mt_rand(0,100000)); // Ping Id
-	$sendPacketBuffer .= "\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78"; // Magic
+	$sendPacketBuffer .= pack("J", mt_rand(0,100000)); // Ping id
+	$sendPacketBuffer .= MAGIC; // Magic
 
 	if(!@fwrite($sock, $sendPacketBuffer)) return false;
 	$receivePacketBuffer = fread($sock, 1024);
@@ -31,6 +33,7 @@ function unconnectedPing(string $host, int $port, &$result) : bool{
 	$sendPingTime = unpack("J", substr($receivePacketBuffer, 1, 8))[1];
 	$serverId = unpack("J", substr($receivePacketBuffer, 9, 8))[1];
 	$magic = substr($receivePacketBuffer, 17, 16);
+	if($magic !== MAGIC) return false;
 	$serverData = substr($receivePacketBuffer, 35, unpack("n", substr($receivePacketBuffer, 33, 2))[1]);
 	$info = explode(';', $serverData);
 	$result = $info;
@@ -41,22 +44,22 @@ if(!unconnectedPing("sg.lbsg.net", 19132, $result)) {
 	print("Failed to get information");
 }else{
 
-	$mcpe = $result[0]; // MCPE
 	// ↓ pmmp and vanilla
+	$mcpe = $result[0]; // MCPE
 	$serverName = $result[1];
-	$unknown1 = $result[2]; // ?
+	$protocol = $result[2];
 	$version = $result[3];
 	$loggedInPlayer = $result[4];
 	$maxPlayer = $result[5];
-	$unknown2 = $result[6]; // ?
+	$unknown2 = $result[6]; // server id?
 	/*
 	// ↓ vanilla only
 	$worldName = $result[7];
 	$gameMode = $result[8];
 	$unknown3 = $result[9]; // ?
-	$unknown4 = $result[10]; // ?
-	$unknown5 = $result[11]; // ?
-	$unknown6 = $result[12]; // ?
+	$ipv4Port = $result[10];
+	$ipv6Port = $result[11];
 	*/
+	var_dump($result);
 	print($serverName . " (" . $loggedInPlayer . "/" . $maxPlayer . ")");
 }
